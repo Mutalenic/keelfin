d841b5bce87d7d0647f38979f2cb449d14cbce28
mutalenic <nicomutale@gmail.com>
2026-02-24 16:15:32 +0300

    Add home icon to categories navigation and fix logout route to use DELETE method

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
 
   devise_scope :user do
-    get '/users/sign_out' => 'devise/sessions#destroy'
+    delete '/users/sign_out' => 'devise/sessions#destroy'
   end
   
   root to: "dashboard#index"
